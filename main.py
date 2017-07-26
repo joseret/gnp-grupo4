60871fcf4c1eb9c90ec561b0c2a4841af62d2ffd
GCP Developer <developer@instance-2.c.gnp-cloudstart-group04.internal>
2017-07-26 16:43:16 +0000

    Cambio en archivos2.0

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type']='text/html'
-        self.response.write('<body style=\'background-color:black color:white\'>Cambio de color Git hub</body>')
+        self.response.write('<body style=\'background-color:black color:white\'>Cambio de color Git hub version 2.0</body>')
 
 
 app = webapp2.WSGIApplication([
