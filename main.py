064e64692079e821733e4e6372f7909d10763ee6
GCP Developer <developer@instance-2.c.gnp-cloudstart-group04.internal>
2017-07-26 16:12:05 +0000

    Cambio en archivos

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type']='text/html'
-        self.response.write('<body style=\'background-color: red\'>Somos equipo 4</body>')
+        self.response.write('<body style=\'background-color:black color:white\'>Cambio de color Git hub</body>')
 
 
 app = webapp2.WSGIApplication([
